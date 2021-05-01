871a9b18592eafe0b0367f7f5d0e3d8738ad3b12
Vitaly Krenel <vitaly.krenel@gmail.com>
2021-05-01 19:33:41 +0300

    Redesign signin button on auth page to be closer to design

--- src/pages/auth-page.tsx
+++ src/pages/auth-page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import FirebaseAuth from 'react-firebaseui/FirebaseAuth'
+import facebookIconUrl from './facebook-signin-provider-icon.svg'
 
 import { Box } from '~/primitives/box'
+import { Text } from '~/primitives/text'
 import { Container } from '~/primitives/container'
 import { IStore } from '~/store/types'
 import { styled } from '~/theming/styled'
@@ -13,8 +15,15 @@ const uiConfig = {
   signInFlow: 'popup',
   // We will display Google and Facebook as auth providers.
   signInOptions: [
-    SignInProviders.GoogleAuthProvider,
-    SignInProviders.FacebookAuthProvider,
+    {
+      provider: SignInProviders.GoogleAuthProvider,
+      fullLabel: 'Войти через Google',
+    },
+    {
+      provider: SignInProviders.FacebookAuthProvider,
+      fullLabel: 'Войти через Facebook',
+      iconUrl: facebookIconUrl,
+    },
   ],
   callbacks: {
     // Avoid redirects after sign-in.
@@ -33,7 +42,8 @@ export const AuthPage = () => {
     <BackgroundContainer>
       <Container uiSaze="lg">
         <BoxContainer alignSelf="center" maxWidth={752} my={4}>
-          <StyledFirebaseAuth
+          <Heading>Вход в аккаунт</Heading>
+          <FirebaseAuthForm
             uiConfig={uiConfig}
             firebaseAuth={firebaseAuth}
           />
@@ -48,9 +58,54 @@ const BackgroundContainer = styled(Box)`
 `
 
 const BoxContainer = styled(Box)`
-  width: 100%;
-  height: calc(100vh - 64px);
+  width: 480px;
+  height: 300px;
+  padding: 48px 36px 36px;
+
   background-color: ${p => p.theme.colors.white};
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2), 0px 0px 1px rgba(9, 30, 66, 0.3);
   border-radius: 6px;
+`
+
+const Heading = styled(Text)`
+  margin: 0 auto 48px;
+
+  color: #171725;
+  font-size: 22px;
+  font-weight: bold;
+`
+
+const FirebaseAuthForm = styled(FirebaseAuth)`
+  & .firebaseui-container,
+  & .firebaseui-card-content,
+  & .firebaseui-idp-list {
+    width: 100% !important;
+    max-width: 100% !important;
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+
+  & .firebaseui-list-item {
+    width: 100%;
+  }
+
+  & .firebaseui-idp-google,
+  & .firebaseui-idp-facebook {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    max-width: 100%;
+
+    padding: 12px;
+    margin-bottom: 12px;
+
+    background-color: #fff !important;
+    box-shadow: none;
+    border-radius: 10px;
+    border: 2px solid #F1F1F5;
+  }
+
+  & .firebaseui-idp-text {
+    color: #696974;
+  }
 `
