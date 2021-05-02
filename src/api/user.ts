4fdd464e2ea9731724b767a7d0a0c42c3d0f8d01
Vitaly Krenel <vitaly.krenel@gmail.com>
2021-05-03 00:33:48 +0300

    Fetch user from firestore when available

---
+++ src/api/user.ts
@@ -0,0 +1,31 @@
+import { User } from '~/domain/user'
+import { getCollectionRef } from '~/infra/firebase/firestore'
+
+const USER_COLLECTION_KEY =  'users'
+
+const getUsersCollection = () => getCollectionRef(USER_COLLECTION_KEY)
+
+const getUser = async (userId: string) => {
+  const userSnapshot = await getUsersCollection().doc(userId).get()
+
+  return userSnapshot.exists ? userSnapshot.data() : null
+}
+
+const checkIsExistingUser = async (userId: string) => {
+  const userSnapshot = await getUsersCollection().doc(userId).get()
+
+  return userSnapshot.exists
+}
+
+const createOrUpdateUser = async (user: User) => {
+  const userDocument = getUsersCollection().doc(user.id)
+  await userDocument.set(user)
+
+  return user
+}
+
+export const UserApi = {
+  getUser,
+  checkIsExistingUser,
+  createOrUpdateUser,
+}
--- src/store/app-store.ts
+++ src/store/app-store.ts
@@ -1,4 +1,5 @@
-import { makeAutoObservable } from 'mobx'
+import { flow, makeAutoObservable } from 'mobx'
+import { UserApi } from '~/api/user'
 import { User, IUserProps } from '~/domain/user'
 class AppStore {
   currentUser: User | null = null
@@ -15,11 +16,20 @@ class AppStore {
     this.currentUser = null
   }
 
-  signIn(userProps: IUserProps) {
+  signIn = flow(function* (this: AppStore, userProps: IUserProps) {
+    const userId = userProps.id
+    const isExistingUser = yield UserApi.checkIsExistingUser(userId)
+
+    if (isExistingUser) {
+      this.currentUser = yield UserApi.getUser(userId)
+      return
+    }
+
     const user = new User(userProps)
 
     this.currentUser = user
-  }
+    yield UserApi.createOrUpdateUser(this.currentUser)
+  })
 }
 
 export { AppStore }
